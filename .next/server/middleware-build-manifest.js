ae3d64e90ea13bc1bb0e359054570bda0f441904
Sottam Aich <sottamaich@gmail.com>
2025-11-23 12:26:54 +0530

    Added the Google drive links for the certificates

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8a89570f8fea94b8.js","static/chunks/fd9d1056-686ec31b8af3a863.js","static/chunks/117-301157f16e1bb94e.js","static/chunks/main-app-4b4ee941410fe4e3.js"],pages:{"/_app":["static/chunks/webpack-8a89570f8fea94b8.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ce9f5e3ee1bd2f3f.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-8a89570f8fea94b8.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ce9f5e3ee1bd2f3f.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- data/certifications.ts
+++ data/certifications.ts
@@ -6,18 +6,21 @@ export type Certification = {
 };
 
 export const certifications: Certification[] = [
-  { title: 'Oracle Cloud Infrastructure 2025 Certified Generative AI Professional', org: 'Oracle', year: '2025', link: '#' },
-  { title: 'Oracle Cloud Infrastructure 2025 Certified Foundations Associate', org: 'Oracle', year: '2025', link: '#' },
-  { title: 'Oracle Cloud Infrastructure 2025 Certified Data Science Professional', org: 'Oracle', year: '2025', link: '#' },
-  { title: 'Machine Learning', org: 'Stanford University', year: '2023', link: '#' },
-  { title: 'Cisco Networking Basics', org: 'Cisco Networking Academy', year: '2023', link: '#' },
-  { title: 'Programming Foundations: Databases', org: 'LinkedIn', year: '2025', link: '#' },
-  { title: 'Oracle Database 19c Basic SQL', org: 'LinkedIn', year: '2025', link: '#' },
-  { title: 'Microsoft Azure Fundamentals (AZ-900)', org: 'LinkedIn', year: '2025', link: '#' },
-  { title: 'IT Security Foundations: Core Concepts', org: 'LinkedIn', year: '2025', link: '#' },
-  { title: 'HTML, CSS, and JavaScript Essential Training', org: 'LinkedIn', year: '2025', link: '#' },
-  { title: 'Linux CentOS 7 Overview and Windows Server 2019 Config', org: 'LinkedIn', year: '2025', link: '#' },
-  { title: 'Learning Virtualization', org: 'LinkedIn', year: '2025', link: '#' }
+  { title: 'Oracle Cloud Infrastructure 2025 Certified Generative AI Professional', org: 'Oracle', year: '2025', link: 'https://drive.google.com/file/d/19eR-ATYZnJhejwDJiM3kDyp_bw-VnmUA/view?usp=drive_link' },
+  { title: 'Oracle Cloud Infrastructure 2025 Certified Foundations Associate', org: 'Oracle', year: '2025', link: 'https://drive.google.com/file/d/1Th1xG_xkRHaN9JkrMC65AWQ1daO8P8g_/view?usp=drive_link' },
+  { title: 'Oracle Cloud Infrastructure 2025 Certified Data Science Professional', org: 'Oracle', year: '2025', link: 'https://drive.google.com/file/d/1NCweu98Y5EXUcNNPgVTKJNmWjSG-AIHg/view?usp=drive_link' },
+  { title: 'Machine Learning', org: 'Stanford University', year: '2023', link: 'https://drive.google.com/file/d/1v_hfQNLRageKeCspA5AiQxwCRdiycP3S/view?usp=drive_link' },
+  { title: 'Cisco Networking Basics', org: 'Cisco Networking Academy', year: '2023', link: 'https://drive.google.com/file/d/1t3o1xTqjx6RX73uydtbtdpMBsB_bdMRq/view?usp=drive_link' },
+  { title: 'Programming Foundations: Databases', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/1N_7aZtwlSWI2634PA60Cp4vpZnSXLdMD/view?usp=drive_link' },
+  { title: 'Oracle Database 19c Basic SQL', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/1zR7hVHyhrcIjJyTpmxz3PpblXyWdlnuz/view?usp=drive_link' },
+  { title: 'Microsoft Azure Fundamentals (AZ-900)', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/19Lf3t3iMlQ6lWfXT6kfWWxGAycgcbts8/view?usp=drive_link' },
+  { title: 'IT Security Foundations: Core Concepts 2019', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/1AzW-_43mTddOjOgv75aH6H1QaVD02Ets/view?usp=drive_link' },
+  { title: 'HTML Essential Training 2020', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/16WjVP2xEdvEbixKx__5sLKTSLZNXXn_0/view?usp=drive_link' },
+  { title: 'CSS Essential Training 2019', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/1lPuU1r4lpS8Tatb1_KPG3axxUkaLJ2z1/view?usp=drive_link' },
+  { title: 'JavaScript Essential Training 2017', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/1r4eWUXJg6WIABFUuhbSysHdoHqOrGYkp/view?usp=drive_link' },
+  { title: 'Linux CentOS 7 Overview and Installation', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/171tpsq8NEcD0BUvWnV7s49TXUb5f9G4K/view?usp=drive_link' },
+  { title: 'Windows Server 2019 Installation and Configuration', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/1gpMa1-FWWBk-5lmJ9s4XWD8QvvawvZg7/view?usp=drive_link' },
+  { title: 'Learning Virtualization 2014', org: 'LinkedIn', year: '2025', link: 'https://drive.google.com/file/d/15xdGXIbAH_lBsyzs24HwWw9Mbt7omUXU/view?usp=drive_link' }
 ];
 
 
